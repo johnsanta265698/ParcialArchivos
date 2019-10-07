0cb279dafc5963a7dd49d7688dbc020308cae73f
John Santa <johnsanta265698@correo.itm.edu.co>
2019-10-07 14:47:56 -0500

    Se agrego metodo borrar a clase controlPais.java

--- src/control/controlPais.java
+++ src/control/controlPais.java
@@ -79,4 +79,33 @@ public class controlPais {
         objArchivos2.renombrarUnArchivo("Paises.tmp", "Paises.txt");
         objArchivos2.borrarUnArchivo("Paises.tmp");
     }
+    
+    public void borrar(){
+        String linea;
+        String[] arrLinea;
+        Archivos objArchivos = new Archivos();
+        objArchivos.abrirArchivoParaLectura("Paises.txt");
+        long nLineas = objArchivos.contarLineas();
+        objArchivos.cerrarArchivoParaLectura();
+        
+        Archivos objArchivos1 = new Archivos();
+        objArchivos1.abrirArchivoParaEscritura("Paises.tmp");
+        objArchivos.abrirArchivoParaLectura("Paises.txt");
+        
+        int i = 1;
+        while (i<=nLineas) {
+            linea = objArchivos.leerUnaLineaTexto();
+            arrLinea = linea.split(",");
+            if (!arrLinea[0].equals(objPais.getCodigo())) 
+                objArchivos1.escribirUnaLineaYDebajo(linea);
+            i+=1;
+        }
+        objArchivos.cerrarArchivoParaLectura();
+        objArchivos1.cerrarArchivoParaEscritura();
+        
+        Archivos objArchivos2= new Archivos();
+        objArchivos2.borrarUnArchivo("Paises.txt");
+        objArchivos2.renombrarUnArchivo("Paises.tmp","Paises.txt");
+        objArchivos2.borrarUnArchivo("Paises.tmp");
+    }
 }
